191d0fe497100a911168d203ff071071ca91b370
Gowsalya-Gopalakrishnan <gowsalyagopalakrishnan1515@gmail.com>
2024-12-28 13:08:57 +0530

    get data is completed

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,5 +1,8 @@
 // import { baseUrl } from "../practicemock2/scripts/baseUrl";
 import { baseUrl } from "./baseUrl.js";
+window.onload=()=>{
+    getdata();
+}
 let quizform = document.getElementById("quizform");
 quizform.addEventListener("submit", async function(){
     event.preventDefault();
@@ -28,3 +31,49 @@ quizform.addEventListener("submit", async function(){
             }
             
 });
+
+function getdata(){
+    fetch(`${baseUrl}/main`)
+    .then((res)=>res.json())
+    .then((data)=>{
+        displaydata(data);
+    })
+}
+
+function displaydata(arr){
+    let cont = document.getElementById("cont");
+    cont.innerHTML="";
+    arr.map((el,i)=>{
+        let card = document.createElement("div")
+        card.setAttribute("class","card");
+
+        let title =document.createElement("h4");
+        title.textContent = `Title : ${el.title}`;
+
+        let option1 = document.createElement("h4");
+        option1.textContent = `Option A : ${el.option1}`;
+
+        let option2 = document.createElement("h4");
+        option2.textContent = `Option B : ${el.option2}`;
+
+        let option3 = document.createElement("h4");
+        option3.textContent = `Option C : ${el.option3}`;
+
+        let option4 = document.createElement("h4");
+        option4.textContent = `Option D : ${el.option4}`;
+
+        let correctanswer = document.createElement("h4");
+        correctanswer.textContent = `Correct Answer : ${el.select}`;
+
+        let reviewStatus = document.createElement("h4");
+        if(reviewStatus === true){
+            reviewStatus.textContent = `Status : Reviewed`;
+        }else{
+            reviewStatus.textContent = `Status : Pending`;
+        }
+
+        card.append(title,option1,option2,option3,option4,correctanswer,reviewStatus);
+        cont.append(card);
+        
+    });
+}
